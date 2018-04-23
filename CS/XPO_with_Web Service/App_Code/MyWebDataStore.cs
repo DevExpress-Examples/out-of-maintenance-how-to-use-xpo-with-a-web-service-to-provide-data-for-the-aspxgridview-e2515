4764ac072c1f47613399ad6268e9c5a1c68bed5a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/XPO_with_Web Service/App_Code/MyWebDataStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Services;
+
+
+[WebServiceBinding(Namespace = WebServiceAttribute.DefaultNamespace)]
+class MyWebDataStore : DevExpress.Xpo.WebServiceDataStore {
+    public MyWebDataStore(string url)
+        : base(url, DevExpress.Xpo.DB.AutoCreateOption.SchemaAlreadyExists) {
+    }
+}
+
---
+++ VB/XPO_with_Web Service/Default.aspx.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Xpo
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Private session As Session = XpoHelper.GetNewSession()
+
+
+	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+		xds.Session = session
+	End Sub
+
+End Class
